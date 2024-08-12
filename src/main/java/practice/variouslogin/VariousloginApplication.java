306d128891a4ab11db13674fb02b54772f9e455d
jhl221123 <jhl221123@naver.com>
2024-08-12 23:14:24 +0900

    Initial commit

---
+++ src/main/java/practice/variouslogin/VariousloginApplication.java
@@ -0,0 +1,13 @@
+package practice.variouslogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VariousloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VariousloginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/variouslogin/VariousloginApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.variouslogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VariousloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
