7151fdb90f6625f75c54abbfd5e6dffb0e4d0a0a
jhl221123 <jhl221123@naver.com>
2024-08-12 23:39:33 +0900

    회원 엔티티 추가

---
+++ src/main/java/practice/variouslogin/jwt/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package practice.variouslogin.jwt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import practice.variouslogin.jwt.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
